0d7870e9a3700700437340e493e02de02f995617
ikun <therealikun@outlook.com>
2025-02-09 21:49:39 +0800

    Site updated: YYYY-02-2025年2月9日 21:49:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/04/Try/","2025/01/27/Post-Front-matte/","2025/01/20/ikun圣地/","2025/01/27/hello-world/"];function toRandomPost(){
+var posts=["2025/01/27/Post-Front-matte/","2025/01/27/hello-world/","2025/01/20/ikun圣地/","2025/02/04/Try/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

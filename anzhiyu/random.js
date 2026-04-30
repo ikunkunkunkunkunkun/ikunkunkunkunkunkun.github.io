80b3b685d34c0881c3f06548a416ca35be742fe3
ikun <therealikun@outlook.com>
2026-04-30 20:08:51 +0800

    Site updated: 2026-04-30 20:08:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/22/SEO博客生产线/","2025/06/18/ai新闻n8n/","2025/01/27/hello-world/","2025/02/13/关于ip检测/","2026/01/22/ai漫剧opal/","2025/02/04/忆昔当年泪不干/","2025/01/20/n8n全自动AI短视频流水线搭建指南/","2026/02/22/部分工具/","2025/01/27/面试基础/"];function toRandomPost(){
+var posts=["2026/03/01/Git实用技巧速查手册/","2026/04/25/Claude Code 多会话协同操作指南/","2026/02/22/SEO博客生产线/","2026/04/29/Hermes-OpenClaw-WorkBuddy-Codex对比/","2025/06/18/ai新闻n8n/","2026/01/22/ai漫剧opal/","2025/01/20/n8n全自动AI短视频流水线搭建指南/","2025/01/27/hello-world/","2025/02/13/关于ip检测/","2025/02/04/忆昔当年泪不干/","2026/02/22/部分工具/","2025/01/27/面试基础/","2026/04/25/🚀 Claude Code 多智能体并行开发 SOP/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
